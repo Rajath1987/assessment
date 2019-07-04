ed2b5c7384455fc00e5a02e99fb560a251bbd060
Rajath1987 <52333850+Rajath1987@users.noreply.github.com>
2019-07-04 18:31:42 +0000

    Update samplescript.js

--- samplescript.js
+++ samplescript.js
@@ -4,4 +4,4 @@ var server = http.createServer(function (request, response) {
   response.end("Assessment Success");
 });
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Assessment Success - Server listening at http://127.0.0.1:8000/");
